b12e4ace038e7e4aa11401ff875d6bf94d1c740a
Rafael Silveria <fetznercomerlato@gmail.com>
2023-03-08 19:08:24 -0300

    Criando a classe Posicao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+
+namespace ChessGame_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new(3, 4);
+
+            Console.WriteLine("Posição : " + p);
+
+
+        }
+    }
+}
+
+
+
