224ac42962cca3401866668b2b89a0152263f2a1
Rafael Silveria <fetznercomerlato@gmail.com>
2023-03-10 19:20:27 -0300

    Tratando erro com excecao personalizada

--- Program.cs
+++ Program.cs
@@ -9,12 +9,12 @@ namespace ChessGame_
     {
         static void Main(string[] args)
         {
+           try
             Tabuleiro tab = new Tabuleiro (8, 8);
 
             tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
             tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
             tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
 
             Tela.ImprimirTabuleiro(tab);
 
